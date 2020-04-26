1fa3b6e2b67f4478f565a884686d961a831435c6
Patrick Menschel <menschel.p@posteo.de>
2020-04-26 21:46:46 +0200

    fix racing conditions in console example - do not set sensor to sleep and do not set working period every time

--- sds011/examples/sds011_console_test.py
+++ sds011/examples/sds011_console_test.py
@@ -3,7 +3,7 @@ from sds011 import SDS011
 port = "/dev/ttyUSB0"
 
 sds = SDS011(port=port,use_database=True)
-sds.set_working_period(rate=5)#one measurment every 5 minutes offers decent granularity and at least a few years of lifetime to the sensor
+#sds.set_working_period(rate=5)#one measurment every 5 minutes offers decent granularity and at least a few years of lifetime to the sensor
 print(sds)
 import csv
 try:
@@ -19,5 +19,5 @@ try:
             
             
 except KeyboardInterrupt:
-    sds.sleep()
+    #sds.sleep()
     sds.__del__()
